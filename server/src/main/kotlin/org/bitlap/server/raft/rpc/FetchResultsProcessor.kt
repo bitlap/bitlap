08406687ba3a1c8c088139079eccc31f4c214795
梦境迷离 <dreamylost@outlook.com>
2021-06-05 22:54:38 +0800

    add driver pb schema

---
+++ server/src/main/kotlin/org/bitlap/server/raft/rpc/FetchResultsProcessor.kt
@@ -0,0 +1,20 @@
+package org.bitlap.server.raft.rpc
+
+import com.alipay.sofa.jraft.rpc.RpcContext
+import com.alipay.sofa.jraft.rpc.RpcProcessor
+import org.bitlap.common.proto.driver.BFetchResults
+
+/**
+ * FetchResults
+ *
+ * @author 梦境迷离
+ * @since 2021/6/5
+ * @version 1.0
+ */
+class FetchResultsProcessor : RpcProcessor<BFetchResults.BFetchResultsReq> {
+    override fun handleRequest(rpcCtx: RpcContext?, request: BFetchResults.BFetchResultsReq?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun interest(): String = BFetchResults.BFetchResultsReq::class.java.name
+}
---
+++ server/src/main/kotlin/org/bitlap/server/raft/rpc/OpenSessionProcessor.kt
@@ -0,0 +1,20 @@
+package org.bitlap.server.raft.rpc
+
+import com.alipay.sofa.jraft.rpc.RpcContext
+import com.alipay.sofa.jraft.rpc.RpcProcessor
+import org.bitlap.common.proto.driver.BOpenSession
+
+/**
+ * OpenSession
+ *
+ * @author 梦境迷离
+ * @since 2021/6/5
+ * @version 1.0
+ */
+class OpenSessionProcessor : RpcProcessor<BOpenSession.BOpenSessionReq> {
+    override fun handleRequest(rpcCtx: RpcContext?, request: BOpenSession.BOpenSessionReq?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun interest(): String = BOpenSession.BOpenSessionReq::class.java.name
+}

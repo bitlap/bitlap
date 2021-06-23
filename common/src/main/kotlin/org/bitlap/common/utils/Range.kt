31a09b1d21c409381b6fcc2d03a3640722cc322b
IceMimosa <chk19940609@gmail.com>
2021-06-23 14:43:45 +0800

    Addd unit tests

--- common/src/main/kotlin/org/bitlap/common/utils/Range.kt
+++ common/src/main/kotlin/org/bitlap/common/utils/Range.kt
@@ -3,7 +3,7 @@ package org.bitlap.common.utils
 import java.io.Serializable
 
 /**
- * Desc: Range utils
+ * Desc: Range utils, reference to guava Range.
  *
  * Mail: chk19940609@gmail.com
  * Created by IceMimosa
@@ -14,6 +14,13 @@ open class Range<C : Comparable<C>>(val lower: LeftCut<C>, val upper: RightCut<C
     constructor(lower: C, upper: C) : this(LeftCut(lower, BoundType.CLOSE), RightCut(upper, BoundType.CLOSE))
 
     companion object {
+
+        /**
+         * [[endpoint], [endpoint]]
+         */
+        fun <C : Comparable<C>> singleton(endpoint: C): Range<C> {
+            return Range(LeftCut(endpoint, BoundType.CLOSE), RightCut(endpoint, BoundType.CLOSE))
+        }
 
         /**
          * ([lower], [upper])
@@ -42,30 +49,92 @@ open class Range<C : Comparable<C>>(val lower: LeftCut<C>, val upper: RightCut<C
         fun <C : Comparable<C>> closedOpen(lower: C, upper: C): Range<C> {
             return Range(LeftCut(lower, BoundType.CLOSE), RightCut(upper, BoundType.OPEN))
         }
+
+        /**
+         * (-∞, [upper])
+         */
+        fun <C : Comparable<C>> lessThan(upper: C): Range<C> {
+            return Range(LeftCut(null, BoundType.INFINITY), RightCut(upper, BoundType.OPEN))
+        }
+
+        /**
+         * (-∞, [upper]]
+         */
+        fun <C : Comparable<C>> atMost(upper: C): Range<C> {
+            return Range(LeftCut(null, BoundType.INFINITY), RightCut(upper, BoundType.CLOSE))
+        }
+
+        /**
+         * ([upper], +∞)
+         */
+        fun <C : Comparable<C>> greaterThan(lower: C): Range<C> {
+            return Range(LeftCut(lower, BoundType.OPEN), RightCut(null, BoundType.INFINITY))
+        }
+
+        /**
+         * [[upper], +∞)
+         */
+        fun <C : Comparable<C>> atLeast(lower: C): Range<C> {
+            return Range(LeftCut(lower, BoundType.CLOSE), RightCut(null, BoundType.INFINITY))
+        }
+
+        /**
+         * (-∞, +∞)
+         */
+        fun <C : Comparable<C>> all(): Range<C> {
+            return Range(LeftCut(null, BoundType.INFINITY), RightCut(null, BoundType.INFINITY))
+        }
     }
 
     operator fun contains(value: C): Boolean {
         return lower <= value && upper >= value
     }
 
+    // TODO: containsAll, encloses, intersection...
+
     fun isValid(): Boolean = lower <= upper
-    fun isSingle(): Boolean = lower.compareTo(upper) == 0
+    fun isEmpty(): Boolean = lower > upper ||
+        (lower.compareTo(upper) == 0 && (lower.boundType == BoundType.OPEN || upper.boundType == BoundType.OPEN))
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Range<*>
+        if (lower != other.lower) return false
+        if (upper != other.upper) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = lower.hashCode()
+        result = 31 * result + upper.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "${lower.describe()}..${upper.describe()}"
+    }
 
     abstract class Cut<C : Comparable<C>>(open val endpoint: C?, open val boundType: BoundType) : Comparable<Cut<C>>, Serializable {
 
         abstract operator fun compareTo(other: C): Int
 
         override operator fun compareTo(other: Cut<C>): Int {
-            if ((other is RightCut && other.boundType == BoundType.INFINITY) || (this is LeftCut && other.boundType == BoundType.INFINITY)) {
+            if ((this is LeftCut && this.isInfinity()) || (other is RightCut && other.isInfinity())) {
                 return -1
             }
-            if ((other is LeftCut && other.boundType == BoundType.INFINITY) || (this is RightCut && other.boundType == BoundType.INFINITY)) {
+            if ((other is LeftCut && other.isInfinity()) || (this is RightCut && this.isInfinity())) {
                 return 1
             }
             return this.endpoint!!.compareTo(other.endpoint!!)
         }
 
+        protected fun isInfinity(): Boolean {
+            return endpoint == null || boundType == BoundType.INFINITY
+        }
+
         abstract fun <R: Comparable<R>> map(func: (C?) -> R?): Cut<R>
+        abstract fun describe(): String
     }
 
     data class LeftCut<C : Comparable<C>>(override val endpoint: C?, override val boundType: BoundType) : Cut<C>(endpoint, boundType) {
@@ -84,6 +153,17 @@ open class Range<C : Comparable<C>>(val lower: LeftCut<C>, val upper: RightCut<C
         override fun <R: Comparable<R>> map(func: (C?) -> R?): LeftCut<R> {
             return LeftCut(func(endpoint), boundType)
         }
+
+        override fun describe(): String {
+            if (endpoint == null || boundType == BoundType.INFINITY) {
+                return "(-\u221e"
+            }
+            return when (boundType) {
+                BoundType.OPEN -> "($endpoint"
+                BoundType.CLOSE -> "[$endpoint"
+                else -> ""
+            }
+        }
     }
 
     data class RightCut<C : Comparable<C>>(override val endpoint: C?, override val boundType: BoundType) : Cut<C>(endpoint, boundType) {
@@ -101,6 +181,17 @@ open class Range<C : Comparable<C>>(val lower: LeftCut<C>, val upper: RightCut<C
 
         override fun <R: Comparable<R>> map(func: (C?) -> R?): RightCut<R> {
             return RightCut(func(endpoint), boundType)
+        }
+
+        override fun describe(): String {
+            if (endpoint == null || boundType == BoundType.INFINITY) {
+                return "+\u221e)"
+            }
+            return when (boundType) {
+                BoundType.OPEN -> "$endpoint)"
+                BoundType.CLOSE -> "$endpoint]"
+                else -> ""
+            }
         }
     }
 

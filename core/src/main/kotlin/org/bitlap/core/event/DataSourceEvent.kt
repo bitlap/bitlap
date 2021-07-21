7573f184c468d47eeaf89664b6fd35d9bfd22e8d
IceMimosa <chk19940609@gmail.com>
2021-07-21 20:06:05 +0800

    Add simple eventbus

---
+++ core/src/main/kotlin/org/bitlap/core/event/DataSourceEvent.kt
@@ -0,0 +1,11 @@
+package org.bitlap.core.event
+
+import org.bitlap.common.BitlapEvent
+
+/**
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2021/7/21
+ */
+
+data class DataSourceCreateEvent(val name: String) : BitlapEvent

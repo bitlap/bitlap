777ece263bc12e4c08478bc0fd52498c3d040fc2
IceMimosa <chk19940609@gmail.com>
2021-12-27 02:55:02 +0800

    Add

--- core/src/test/kotlin/org/bitlap/core/test/sql/UdfTest.kt
+++ core/src/test/kotlin/org/bitlap/core/test/sql/UdfTest.kt
@@ -1,11 +1,24 @@
 package org.bitlap.core.test.sql
 
+import org.bitlap.core.sql.udf.FunctionRegistry
 import org.bitlap.core.test.base.BaseLocalFsTest
 import org.bitlap.core.test.base.SqlChecker
 
 class UdfTest : BaseLocalFsTest(), SqlChecker {
 
     init {
+
+        "test lambda udf registry" {
+            FunctionRegistry.register("test_hello") { s: String ->
+                "hello $s"
+            }
+            // TODO: not support, shoud clean kotlin lambda like apache spark
+            // for example, kotlin compiler generate class is: org.bitlap.core.test.sql.UdfTest$1$1
+//            checkRows(
+//                "select test_hello('a')",
+//                listOf(listOf("hello a"))
+//            )
+        }
 
         "test hello udf" {
             checkRows(

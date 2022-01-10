9cb15b01090e63ebedeccc92c3117a35a45efda8
IceMimosa <chk19940609@gmail.com>
2022-01-10 13:52:38 +0800

    Add assembly

---
+++ dev/make-tarball.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)/../
+
+# init dist directory
+mkdir -p dist/
+find dist/ -name 'bitlap*.tar.gz' | xargs rm -f
+
+# make tar
+TAR_FILE="server/target/bitlap*.tar.gz"
+cmd="./mvnw clean package -DskipTests -Passembly -am -pl server"
+echo "========================================================================================================================================"
+echo "==================  🔥 package start: ${cmd}  ============================"
+echo "========================================================================================================================================"
+eval ${cmd}
+
+# move to dist directory
+if [[ $? -eq 0 ]]; then
+  mv ${TAR_FILE} dist/
+  echo "=============================================================================="
+  echo "===============  🎉 package end in dist directory !!!  ======================="
+  echo "=============================================================================="
+fi

982c0ab08153668c3c5fc1e8e5ecfc4b4ed02dae
梦境迷离 <dreamylost@outlook.com>
2022-04-21 21:29:42 +0800

    zio

--- network/src/main/scala/org/bitlap/network/RpcClient.scala
+++ network/src/main/scala/org/bitlap/network/RpcClient.scala
@@ -1,15 +1,17 @@
 /* Copyright (c) 2022 bitlap.org */
 package org.bitlap.network
 
-import io.grpc.{ ManagedChannelBuilder, Status }
-import org.bitlap.network.driver.proto.BCloseSession.{ BCloseSessionReq, BCloseSessionResp }
-import org.bitlap.network.driver.proto.BExecuteStatement.{ BExecuteStatementReq, BExecuteStatementResp }
-import org.bitlap.network.driver.proto.BOpenSession.{ BOpenSessionReq, BOpenSessionResp }
-import org.bitlap.network.driver.proto.{ BStatus, BStatusCode }
+import io.grpc.{ManagedChannelBuilder, Status}
+import org.bitlap.network.driver.proto.BCloseSession.{BCloseSessionReq, BCloseSessionResp}
+import org.bitlap.network.driver.proto.BExecuteStatement.{BExecuteStatementReq, BExecuteStatementResp}
+import org.bitlap.network.driver.proto.BOpenSession.{BOpenSessionReq, BOpenSessionResp}
+import org.bitlap.network.driver.proto.{BStatus, BStatusCode}
 import org.bitlap.network.driver.service.ZioService.DriverServiceClient
 import org.bitlap.tools.apply
 import scalapb.zio_grpc.ZManagedChannel
-import zio.{ Layer, _ }
+import zio.{Layer, _}
+
+import scala.reflect.{ClassTag, classTag}
 
 /**
  * rpc client to wrapper sofa client
@@ -20,36 +22,43 @@ class RpcClient(uri: String, port: Int) {
   private lazy val runtime = zio.Runtime.global
 
   val clientLayer: Layer[Throwable, DriverServiceClient] = DriverServiceClient.live(
-    ZManagedChannel.apply(ManagedChannelBuilder.forAddress(uri, port))
+    ZManagedChannel(ManagedChannelBuilder.forAddress(uri, port))
   )
+  
+  def openSession(request: BOpenSessionReq): IO[Throwable, BOpenSessionResp] =
+    DriverServiceClient.openSession(request)
+      .mapError(st => new Throwable(exception(st)))
+      .provideLayer(clientLayer)
 
-  def openSession(request: BOpenSessionReq): IO[Status, BOpenSessionResp] =
-    DriverServiceClient.openSession(request).provideLayer(clientLayer)
+  def closeSession(request: BCloseSessionReq): ZIO[Any, Throwable, BCloseSessionResp] = {
+    DriverServiceClient.closeSession(request)
+      .mapError(st => new Throwable(exception(st)))
+      .provideLayer(clientLayer)
+  }
 
-  def closeSession(request: BCloseSessionReq): ZIO[Any, Status, BCloseSessionResp] =
-    DriverServiceClient.closeSession(request).provideLayer(clientLayer)
-
-  def executeStatement(request: BExecuteStatementReq): ZIO[Any, Status, BExecuteStatementResp] =
-    DriverServiceClient.executeStatement(request).provideLayer(clientLayer)
+  def executeStatement(request: BExecuteStatementReq): ZIO[Any, Throwable, BExecuteStatementResp] =
+    DriverServiceClient.executeStatement(request)
+      .mapError(st => new Throwable(exception(st)))
+      .provideLayer(clientLayer)
 
   def syncOpenSession(request: BOpenSessionReq): BOpenSessionResp =
     blocking {
       openSession(request)
-    } { t =>
+    } { t: BOpenSessionResp =>
       verifySuccess(t.getStatus, t)
     }
 
   def syncCloseSession(request: BCloseSessionReq): BCloseSessionResp =
     blocking {
       closeSession(request)
-    } { t =>
+    } { t: BCloseSessionResp =>
       verifySuccess(t.getStatus, t)
     }
 
   def syncExecuteStatement(request: BExecuteStatementReq): BExecuteStatementResp =
     blocking {
       executeStatement(request)
-    } { t =>
+    } { t:BExecuteStatementResp =>
       verifySuccess(t.getStatus, t)
     }
 
@@ -63,7 +72,12 @@ class RpcClient(uri: String, port: Int) {
       t
     }
 
-  @inline private def blocking[T, Z <: zio.ZIO[Any, Status, T]](action: => Z)(t: T => T): T =
-    t.apply(runtime.unsafeRun(action))
+  @inline private def blocking[T:ClassTag,Z:ClassTag](action: => Z)(t: T => T): T = {
+    classTag[T].runtimeClass match {
+      case clazz if clazz.isAssignableFrom(classOf[ZIO[Any, Throwable, T]]) =>
+        t.apply(runtime.unsafeRun(action.asInstanceOf[ZIO[Any, Throwable, T]]))
+    }
+  }
 
+  @inline private def exception(st: Status) = NetworkException(st.getCode.value(),st.getCode.toStatus.getDescription)
 }

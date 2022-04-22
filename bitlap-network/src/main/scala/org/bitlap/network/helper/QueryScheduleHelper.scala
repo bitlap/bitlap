70fbc0fff40754a34f7b00f297444b8f493e94a9
梦境迷离 <dreamylost@outlook.com>
2022-04-22 11:19:14 +0800

    Update QueryScheduleHelper.scala

--- bitlap-network/src/main/scala/org/bitlap/network/helper/QueryScheduleHelper.scala
+++ bitlap-network/src/main/scala/org/bitlap/network/helper/QueryScheduleHelper.scala
@@ -1,6 +1,7 @@
 /* Copyright (c) 2022 bitlap.org */
 package org.bitlap.network.helper
 
-import org.bitlap.network.{ Identity, NetworkHelper }
+import org.bitlap.network.Identity
+import org.bitlap.network.NetworkHelper
 
 trait QueryScheduleHelper extends NetworkHelper[Identity] {}

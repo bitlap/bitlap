e86ed27080a7e120b34a62fa65c5394e491c4dd8
梦境迷离 <dreamylost@outlook.com>
2022-04-28 00:38:55 +0800

    add testkit draft

--- integration-test/src/main/scala/org/bitlap/it/EmbedBitlapServer.scala
+++ integration-test/src/main/scala/org/bitlap/it/EmbedBitlapServer.scala
@@ -1,11 +1,11 @@
 /* Copyright (c) 2022 bitlap.org */
 package org.bitlap.it
 
-import org.bitlap.testkit.server.MockLiveWithBackendServer
+import org.bitlap.testkit.server.MockServer
 import zio.console.Console
 import zio.{ ExitCode, URIO }
 
-object EmbedBitlapServer extends MockLiveWithBackendServer {
+object EmbedBitlapServer extends MockServer {
 
   override def port: Int = 23333
   override def run(args: List[String]): URIO[zio.ZEnv with Console, ExitCode] = super.run(args)

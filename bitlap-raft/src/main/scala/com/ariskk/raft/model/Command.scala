c6598bbf5f24d6beaa90af7dca1aa0156bb5dd12
梦境迷离 <dreamylost@outlook.com>
2022-10-27 22:23:39 +0800

    fix test

--- bitlap-raft/src/main/scala/com/ariskk/raft/model/Command.scala
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) 2022 bitlap.org */
-package com.ariskk.raft.model
-
-sealed trait Command extends Serializable
-object Command {
-  trait ReadCommand extends Command
-
-  trait WriteCommand extends Command
-}

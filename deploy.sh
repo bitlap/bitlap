95af50772851c4d5601a1b3229b993512032d33d
梦境迷离 <dreamylost@outlook.com>
2022-11-19 00:23:57 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ version=$1
 sh dev/make-tarball.sh
 cd dist
 scp bitlap-$1.tar.gz root@your_ip:/root/bitlap-$1.tar.gz
-ssh root@your_ip "cd /root; tar -zxvf bitlap-$1.tar.gz; nohup ./bitlap-$1.tar/bin/bitlap server restart > /dev/null 2>&1"
+ssh root@your_ip "cd /root; tar -zxvf bitlap-$1.tar.gz; cp bitlap-$1/conf/initFileForTest.sql bitlap-$1/initFileForTest.sql; cd bitlap-$1; nohup bin/bitlap server restart > /dev/null 2>&1"

a71ac0bf672dd4d2e1273225a7bf969e800fd037
IceMimosa <chk19940609@gmail.com>
2022-11-24 19:37:54 +0800

    Add http&ftp data load (#352)
    * Add http&ftp data load

---
+++ bitlap-core/src/main/kotlin/org/bitlap/core/hadoop/HttpFileSystem.kt
@@ -0,0 +1,9 @@
+/* Copyright (c) 2022 bitlap.org */
+package org.bitlap.core.hadoop
+
+/**
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2022/11/23
+ */
+class HttpFileSystem : AbstractHttpFileSystem("http")
---
+++ bitlap-core/src/test/kotlin/org/bitlap/core/test/utils/FtpUtils.kt
@@ -0,0 +1,46 @@
+/* Copyright (c) 2022 bitlap.org */
+package org.bitlap.core.test.utils
+
+import org.apache.ftpserver.FtpServer
+import org.apache.ftpserver.FtpServerFactory
+import org.apache.ftpserver.listener.ListenerFactory
+import org.apache.ftpserver.usermanager.impl.BaseUser
+import org.apache.ftpserver.usermanager.impl.WritePermission
+import org.bitlap.common.logger
+import java.net.ServerSocket
+
+/**
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2022/11/23
+ */
+object FtpUtils {
+
+    private val log = logger { }
+
+    fun start(home: String): Pair<FtpServer, Int> {
+        val port = ServerSocket(0).use {
+            it.localPort
+        }
+        log.info { "ftp port: $port, home: $home" }
+        return start(port, home) to port
+    }
+
+    fun start(port: Int, home: String): FtpServer {
+        val server = FtpServerFactory()
+        val factory = ListenerFactory().apply {
+            this.serverAddress = "127.0.0.1"
+            this.port = port
+        }
+        server.addListener("default", factory.createListener())
+        server.userManager.save(
+            BaseUser().apply {
+                this.name = "bitlap"
+                this.password = "bitlap"
+                this.homeDirectory = home
+                this.authorities = listOf(WritePermission())
+            }
+        )
+        return server.createServer().apply { this.start() }
+    }
+}

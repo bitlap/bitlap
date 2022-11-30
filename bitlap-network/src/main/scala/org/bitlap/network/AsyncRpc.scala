dcfe01d9151c88c4af0ea9ce795a63678261927c
梦境迷离 <dreamylost@outlook.com>
2022-11-30 22:16:22 +0800

    catch ex

--- bitlap-network/src/main/scala/org/bitlap/network/AsyncRpc.scala
+++ bitlap-network/src/main/scala/org/bitlap/network/AsyncRpc.scala
@@ -1,9 +1,13 @@
 /* Copyright (c) 2022 bitlap.org */
 package org.bitlap.network
 
+import org.bitlap.network.NetworkException.RpcException
 import org.bitlap.network.handles._
 import org.bitlap.network.models._
 import zio._
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /** 函数式异步RPC API，客户端和服务端通用，基于 zio 1.0
  *
@@ -15,16 +19,27 @@ import zio._
  */
 trait AsyncRpc extends Rpc[Task] { self =>
 
+  private lazy val runtime = zio.Runtime.default
+  private lazy val timeout = Duration("30s")
+
   override def pure[A](a: A): Task[A] = Task.succeed(a)
 
   override def map[A, B](fa: self.type => Task[A])(f: A => B): Task[B] = fa(this).map(f)
 
   override def flatMap[A, B](fa: self.type => Task[A])(f: A => Task[B]): Task[B] = fa(this).flatMap(f)
 
-  def sync[T, E <: Throwable, Z <: ZIO[_, _, _]](action: self.type => Z)(implicit
-    runtime: zio.Runtime[Any] = zio.Runtime.default
-  ): T =
-    runtime.unsafeRun(action(this).asInstanceOf[ZIO[Any, E, T]])
+  def sync[T, E <: Throwable, Z <: ZIO[_, _, _]](
+    action: self.type => Z
+  )(implicit runtime: zio.Runtime[Any] = runtime): T =
+    try {
+      val cancelableFuture = runtime.unsafeRunToFuture(action(this).asInstanceOf[ZIO[Any, E, T]])
+      Await.result(cancelableFuture.future, timeout)
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        throw RpcException(msg = "Sync future exception", cause = Option(e))
+        null.asInstanceOf[T]
+    }
 
   def when[A, E <: Throwable](predicate: => Boolean, exception: => E, fa: self.type => Task[A]): Task[A] =
     if (predicate) {

25fecce9d2cc9500e8f62a6ae3ad67715145019c
梦境迷离 <dreamylost@outlook.com>
2023-03-19 02:08:50 +0800

    fix session

--- bitlap-server/src/main/scala/org/bitlap/server/http/HttpServiceLive.scala
+++ bitlap-server/src/main/scala/org/bitlap/server/http/HttpServiceLive.scala
@@ -51,8 +51,8 @@ final class HttpServiceLive {
              |}
              |""".stripMargin)
     } finally {
-//      stmt.close()
-//      conn.close()
+      stmt.close()
+      conn.close()
     }
   }
 

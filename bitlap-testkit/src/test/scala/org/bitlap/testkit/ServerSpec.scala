bcefec0b601fe0370dd2b6d599668cbc7e3375e6
梦境迷离 <dreamylost@outlook.com>
2023-07-21 21:02:49 +0800

    fix ci (#523)

--- bitlap-testkit/src/test/scala/org/bitlap/testkit/ServerSpec.scala
+++ bitlap-testkit/src/test/scala/org/bitlap/testkit/ServerSpec.scala
@@ -18,11 +18,12 @@ class ServerSpec extends CSVUtils {
   given Connection = DriverManager.getConnection("jdbc:bitlap://localhost:23333/default")
 
   // 每个测试都会执行一次，需要修改！
+  val server = new Thread {
+    override def run(): Unit = EmbedBitlapServer.main(scala.Array.empty)
+  }
+
   @Before
   def startServer(): Unit = {
-    val server = new Thread {
-      override def run(): Unit = EmbedBitlapServer.main(scala.Array.empty)
-    }
     server.setDaemon(true)
     server.start()
     Thread.sleep(3000L)
@@ -53,13 +54,13 @@ class ServerSpec extends CSVUtils {
     assert(ret1.nonEmpty)
     println(ret1)
 
-//    sql"create database if not exists $database"
-//    sql"use $database"
+    //    sql"create database if not exists $database"
+    //    sql"use $database"
 
-//    val showResult = ResultSetX[TypeRow1[String]](sql"show current_database").fetch()
-//    println(database)
-//    println(showResult.map(_.values))
-//    assert(showResult.nonEmpty && showResult.exists(_.values.contains(database)))
+    //    val showResult = ResultSetX[TypeRow1[String]](sql"show current_database").fetch()
+    //    println(database)
+    //    println(showResult.map(_.values))
+    //    assert(showResult.nonEmpty && showResult.exists(_.values.contains(database)))
   }
 
 }

554e5466f4fc31eabe3df78e4d79ae4ff4605afe
梦境迷离 <dreamylost@outlook.com>
2023-07-21 19:53:06 +0800

    fix

--- bitlap-testkit/src/test/scala/org/bitlap/testkit/ServerSpec.scala
+++ bitlap-testkit/src/test/scala/org/bitlap/testkit/ServerSpec.scala
@@ -18,9 +18,7 @@ class ServerSpec extends CSVUtils {
   given Connection = DriverManager.getConnection("jdbc:bitlap://localhost:23333/default")
 
   // 每个测试都会执行一次，需要修改！
-  val server = new Thread {
-    override def run(): Unit = EmbedBitlapServer.main(scala.Array.empty)
-  }
+  val server: Thread = new Thread(() => EmbedBitlapServer.main(scala.Array.empty))
 
   @Before
   def startServer(): Unit = {

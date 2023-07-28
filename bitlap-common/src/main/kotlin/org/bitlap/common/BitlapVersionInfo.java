d62a3f3ae117fcd1ace02ad446fbdb924e06e90d
梦境迷离 <dreamylost@outlook.com>
2023-07-28 21:10:44 +0800

    generate version (#534)

--- bitlap-common/src/main/kotlin/org/bitlap/common/BitlapVersionInfo.java
+++ bitlap-common/src/main/kotlin/org/bitlap/common/BitlapVersionInfo.java
@@ -1,52 +1,23 @@
 package org.bitlap.common;
 
+import com.typesafe.config.*;
+
 /**
  * @author 梦境迷离
  * @version 1.0, 2023/3/18
  */
 public class BitlapVersionInfo {
-    private static Package myPackage;
-    private static BitlapVersionAnnotation version;
+
+    private static String version;
 
     static {
-        myPackage = BitlapVersionAnnotation.class.getPackage();
-        version = myPackage.getAnnotation(BitlapVersionAnnotation.class);
+        Config config = ConfigFactory.load("maven-version.properties");
+        version = config.getString("maven.package.version");
     }
 
-    static Package getPackage() {
-        return myPackage;
-    }
 
     public static String getVersion() {
-        return version != null ? version.version() : "Unknown";
-    }
-
-    public static String getShortVersion() {
-        return version != null ? version.shortVersion() : "Unknown";
-    }
-
-    public static String getRevision() {
-        return version != null ? version.revision() : "Unknown";
-    }
-
-    public static String getDate() {
-        return version != null ? version.date() : "Unknown";
-    }
-
-    public static String getUser() {
-        return version != null ? version.user() : "Unknown";
-    }
-
-    public static String getUrl() {
-        return version != null ? version.url() : "Unknown";
-    }
-
-    public static String getSrcChecksum() {
-        return version != null ? version.srcChecksum() : "Unknown";
-    }
-
-    public static String getBuildVersion() {
-        return BitlapVersionInfo.getVersion() + " from " + BitlapVersionInfo.getRevision() + " by " + BitlapVersionInfo.getUser() + " source checksum " + BitlapVersionInfo.getSrcChecksum();
+        return version != null ? version : "Unknown";
     }
 
 }
---
+++ bitlap-common/src/test/kotlin/org/bitlap/common/test/BitlapVersionTest.kt
@@ -0,0 +1,19 @@
+/* Copyright (c) 2023 bitlap.org */
+package org.bitlap.common.test
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.bitlap.common.BitlapVersionInfo
+
+/**
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020/12/22
+ */
+class BitlapVersionTest : StringSpec({
+
+    "test bitlap version generate" {
+        val version = BitlapVersionInfo.getVersion()
+        version.substring(0, 1) shouldBe "0"
+    }
+})
